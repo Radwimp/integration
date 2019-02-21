0344a2f65a40ef22c05cc60c08a023c34364beb1
Radwimp <geass23621@gmail.com>
2019-02-21 17:43:07 +0200

    Feature: add login fn and Cypress.env

--- src/support/commands.js
+++ src/support/commands.js
@@ -24,38 +24,16 @@
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
-// Cypress.Commands.add('login', () => {
-//     cy.request ({
-//         method: 'POST',
-//         url: 'http://auth.wb1.helioscloud.com/api/v1/sign_in',
-//         body: {
-//             login: 'admin@barong.io',
-//             password: 'Qwerty123',
-//         }
-//     })
-//     .then((resp) => {
-//         window.localStorage.setItem('jwt', resp.body.user.token)
-//     })
-// })
-
 Cypress.Commands.add('login', () => {
-  cy.fixture('peatio').then((peatio) => {
-
-    cy.visit(peatio.host)
-
-    cy.get('a.btn').then((button) => {
-      if ((/auth\/barong/).test(button[0].href)) {
-        cy.visit(button[0].href)
-        cy.fixture('user.json').then((user) => {
-          cy.get('#account_email').type(user.login).should('have.value', user.login)
-
-          cy.contains('.btn-success', 'Submit').click()
-
-          cy.get('#account_password').type(user.password)
-
-          cy.contains('.btn-success', 'Submit').click()
-        })
-      }
+    cy.request ({
+        method: 'POST',
+        url: Cypress.env("login_url"),
+        body: {
+            email: Cypress.env("login"),
+            password: Cypress.env("password"),
+        }
     })
-  })
-})
+    .then((resp) => {
+      window.localStorage.setItem('id_token', resp.body)
+    })
+});
